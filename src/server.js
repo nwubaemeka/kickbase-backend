99ef8223d57a0c07aaabf383d6f1d146fae60d68
Uranium <strindlei@gmail.com>
2026-05-07 23:42:37 +0100

    Fix proxy trust for Render

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ const io = new Server(server, {
 
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors({ origin: process.env.FRONTEND_URL || '*', credentials: true }));
+app.set('trust proxy', 1);
 app.use(express.json({ limit: '50kb' }));
 
 const apiLimiter = rateLimit({
